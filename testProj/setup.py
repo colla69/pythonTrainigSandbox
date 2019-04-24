5a8f70651dce90678ad3573fa60b741909814fca
colla69 <colla69@gmail.com>
2019-04-24 19:04:17 +0200

    first commit

---
+++ testProj/setup.py
@@ -0,0 +1,10 @@
+
+
+from testProj.nester import print_lol
+
+movies = [
+    "The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91,
+        ["Graham Chapman",
+            ["Michael Palin", "John Cleese", "Terry Gilliam", "Eric Idle", "Terry Jones"]]]
+
+print_lol(movies)
